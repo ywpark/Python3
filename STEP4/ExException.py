bef80aef6106089766d5bfeda810e1adfe832eb2
ywpark <ywpark0416@gmail.com>
2017-07-27 12:13:38 +0900

    4주차내용

---
+++ STEP4/ExException.py
@@ -0,0 +1,65 @@
+#math 모듈에서 pi만 쓰겟다
+#from math import pi
+
+num = 1
+num2 = 0
+
+#print(num/num2)
+
+
+# try:
+#     print(num/num2)
+#     print('success')
+# except ZeroDivisionError:
+#     print('zeroError')
+# except :
+#     print('except')
+
+
+# try:
+#     print('hi' + num)
+# except ZeroDivisionError :
+#     print('zeroError')
+# except (TypeError, ValueError) :
+#     print('typeError or valueError')
+
+
+# try:
+#     print('Hello')
+#     print(1/0)
+# except ZeroDivisionError:
+#     print('zeroError')
+# finally:
+#     print('This code will run no matter what')
+
+
+# try:
+#     print(1)
+#     print(10/0)
+# except ZeroDivisionError :
+#     print(unknown_var)
+# finally:
+#     print('This is executed last')
+
+
+# print(1)
+# raise ValueError
+# print(2)
+
+
+# try:
+#     print(1/0)
+# except ZeroDivisionError :
+#     raise TypeError
+
+
+# raise NameError('세부정보')
+
+# print(1)
+# assert 2 + 2 == 4
+# print(2)
+# assert 1 + 1 == 3
+# print(3)
+
+tmp = -10
+assert tmp > 0, 'Colder than absolute zero!'
---
+++ STEP4/ExFile.py
@@ -0,0 +1,59 @@
+
+#기본적인 파일 오픈
+#myfile = open('test.txt')
+
+#write mode
+#새로작성
+#open('test.txt', 'w')
+#마지막 부분에 추가적으로 작성
+#open('test.txt', 'a')
+
+#read mode
+#open('test.txt', 'r')
+
+#binary write mode
+#open('test.txt', 'wb')
+
+#마직막은 항상 닫기
+#close()
+
+# file = open('test.txt', 'r')
+# cont = file.read()
+# print(cont)
+# file.close()
+
+
+# file = open('test.txt', 'r')
+# print(file.read(1))
+# print(file.read(2))
+# print(file.read(3))
+# print(file.read(10))
+# file.close()
+
+# file = open('test.txt', 'r')
+# #print(file.readlines())
+# for line in file:
+#     print(line)
+# file.close()
+
+
+# # w 모드는 없으면 create 한다
+# file = open('test1.txt', 'w')
+# file.write('This has been written to a file')
+# file.close()
+#
+# file = open('test1.txt', 'r')
+# print(file.read())
+# file.close()
+
+# msg = 'Hello world!!!!'
+# file = open('test2.txt', 'w')
+# amount_written = file.write(msg)
+# print(amount_written)
+# file.close()
+
+try:
+    f = open('test2.txt')
+    print(f.read())
+finally:
+    f.close()
